e086e0c7a32276764e81cc3e1de517246e62fbbb
DanielTry-ISA <danieltry3.martinez@gmail.com>
2026-05-14 01:54:16 -0500

    Fondo de Partículas

---
+++ frontend/app.js
@@ -0,0 +1,34 @@
+particlesJS("particles-js", {
+    //Configuración de las partículas
+  particles: {
+    // cantidad de estrellas
+    number: {
+      value: 150
+    },
+      
+    color: {
+      value: "#f8efef"
+    },
+    shape:{
+    type: "circle"
+    },
+
+    size:{
+        value: 2
+    },
+
+
+    move: {
+      enable: true,
+      speed: 0.5
+    },
+
+    line_linked: {
+
+      /*
+        false = sin líneas
+      */
+      enable: true
+    }
+  }
+});
