a55de1b5212f8c764a7ab762b9d5fda8ed373c0b
DanielTry-ISA <danieltry3.martinez@gmail.com>
2026-05-14 15:34:35 -0500

    Barra de chat añadida

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 from groq import Groq
 from fastapi import FastAPI
 from dotenv import load_dotenv
+from pydantic import BaseModel
 import os
 
 apikey = None
+
+#Convertir luego a unm arreglo de modelos
+model = 'llama-3.3-70b-versatile'
 
 def load_env():
     global apikey
@@ -12,10 +16,22 @@ def load_env():
 
 load_env()
 
+
 Client = Groq(api_key=apikey)
 
 app = FastAPI()
 
+class ChatRequest(BaseModel):
+    message: str
+
 @app.get("/")
 async def root():
     return {"message": "api funciona!"}
+
+
+
+
+
+
+
+
