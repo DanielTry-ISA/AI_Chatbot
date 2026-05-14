0f216a0284a5e97366d82e7a8de32f8bffb0eba7
DanielTry-ISA <danieltry3.martinez@gmail.com>
2026-05-14 02:23:52 -0500

    api funcional

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,21 @@
 from groq import Groq
-from fastapi import FastApi
+from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 
+apikey = None
 
+def load_env():
+    global apikey
+    load_dotenv()
+    apikey = os.getenv("GROQ_API_KEY")
+
+load_env()
+
+Client = Groq(api_key=apikey)
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "api funciona!"}
